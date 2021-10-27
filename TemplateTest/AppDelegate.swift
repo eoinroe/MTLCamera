dfcd0cea4bc49c4593fa204f0183d1f24b10a534
Eoin Roe <eoinroe1@gmail.com>
2021-10-27 15:15:04 +0100

    Initial Commit

---
+++ TemplateTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TemplateTest
+//
+//  Created by Eoin Roe on 27/10/2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
